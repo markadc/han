b1171fe074788fc0f1c524d2af03d5bcb8a0f6c7
markadc <markadc@126.com>
2025-02-13 18:24:28 +0800

    又一版本的请求

---
+++ crawler/crawler.go
@@ -0,0 +1,81 @@
+package crawler
+
+import (
+	"kss/requests"
+	"net/http"
+	"time"
+)
+
+type Crawler struct {
+	Name    string
+	proxy   string
+	timeout time.Duration
+}
+
+func NewCrawler(name string) *Crawler {
+	return &Crawler{Name: name}
+}
+
+func (c *Crawler) GetProxy() string {
+	return c.proxy
+}
+
+func (c *Crawler) GetTimeout() time.Duration {
+	return c.timeout
+}
+
+func (c *Crawler) SetProxy(proxy string) {
+	c.proxy = proxy
+}
+
+func (c *Crawler) SetTimeout(timeout time.Duration) {
+	c.timeout = timeout
+}
+
+func (c *Crawler) Get(url string, headers, params map[string]string) (*Response, error) {
+	req, err := requests.MakeGetRequest(url, headers, params)
+	if err != nil {
+		return nil, err
+	}
+	return c.Go(req)
+
+}
+
+func (c *Crawler) Post(api string, headers map[string]string, data []byte) (*Response, error) {
+	req, err := requests.MakePostRequest(api, headers, data)
+	if err != nil {
+		return nil, err
+	}
+	return c.Go(req)
+}
+
+func (c *Crawler) PostForm(api string, headers, formData map[string]string) (*Response, error) {
+	req, err := requests.MakePostFormRequest(api, headers, formData)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	return c.Go(req)
+}
+
+func (c *Crawler) Go(req *http.Request) (*Response, error) {
+	if c.proxy == "" {
+		client := &http.Client{Timeout: c.timeout}
+		return GetResponse(req, client)
+	} else {
+		client, err := requests.CreateProxyClient(c.proxy, c.timeout)
+		if err != nil {
+			return nil, err
+		}
+		return GetResponse(req, client)
+	}
+}
+
+func GetResponse(req *http.Request, client *http.Client) (*Response, error) {
+	body, err := requests.Send(req, client)
+	if err != nil {
+		return nil, err
+	}
+	res := &Response{Content: body, Text: string(body)}
+	return res, nil
+}
---
+++ crawler/response.go
@@ -0,0 +1,36 @@
+package crawler
+
+import (
+	"encoding/json"
+	"kss"
+	"log"
+)
+
+// Response 响应结构体
+type Response struct {
+	Content []byte
+	Text    string
+}
+
+// JSON 获取JSON格式数据
+func (r Response) JSON() (kss.A, error) {
+	var jsonData kss.A
+	err := json.Unmarshal(r.Content, &jsonData)
+	if err != nil {
+		return nil, err
+	}
+	return jsonData, nil
+}
+
+// JsonStringify JSON字符串格式化，优化打印显示
+func (r Response) JsonStringify() string {
+	jsonData, err := r.JSON()
+	if err != nil {
+		panic(err)
+	}
+	bs, err := json.MarshalIndent(jsonData, "", "  ")
+	if err != nil {
+		log.Fatalf("JSON serialization failed: %v", err)
+	}
+	return string(bs)
+}
